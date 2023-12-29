b88d058bdcc236247df53d8fa868ec6fa382c3e5
Alp Eren Kırgilboğa <139857317+Alpp0@users.noreply.github.com>
2023-12-29 16:03:04 +0300

    2. Hafta
    Bugün reactta input ve button örneklerini  basitçe işledik.

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+const App = () => {
+  const handleClick = () => {
+    alert("Merhaba");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Click Button" onPress={handleClick} />
+      <TouchableOpacity onPress={handleClick}>
+        <Text>Click TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight
+        underlayColor="#DDDDDD"
+        onPress={() => alert("Pressed!")}
+      >
+        <Text>Click TouchableHighlight</Text>
+      </TouchableHighlight>
+      <TouchableWithoutFeedback onPress={() => alert("Pressed!")}>
+        <Text>Click TouchableWithoutFeedback</Text>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default App;
